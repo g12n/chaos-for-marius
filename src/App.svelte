6894cf7d03d2baf9a5c35c3f28080fcaa1a381b3
mge <michael@gehrmann-design.de>
2020-02-11 21:50:57 +0100

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+	import Marius from './Marius.svelte'
+	 import 'file-saver';
+	 	import math from 'canvas-sketch-util/math';
+
+	let name;
+	let size = 1024;
+	let steps=16;
+	let canvas;
+	let hue=100;
+	let spread = 0.15;
+	let angle = 0.25 * Math.PI;
+	
+	let save = () =>{
+		let now = new Date();
+		let fileName = `${artworks[art].fileName}-${name}.png`
+		canvas.toBlob(function(blob) {
+  	  		saveAs(blob, fileName);
+		});
+	}
+
+</script>
+
+<style>
+	:global(body){
+		padding: 0; 
+		margin: 0;
+		background: #456;
+		color: #fff;
+	}
+	:global(canvas){
+		display:block;
+		width:70vmin;
+		height: 70vmin;
+		margin: 5vmin  auto;
+		border: 1vmin solid #fff;
+		box-shadow: 1vmin 2vmin 30px rgba(0,0,0,0.4)
+	}
+	.ui{
+		display: flex; 
+		flex-direction: column;
+		padding: 2rem;
+	}
+	p{font-size: 1.2rem; text-align: center;}
+</style>
+
+<Marius size={size} angle={angle} spread={spread} bind:canvas={canvas} bind:name={name} steps={steps}/>
+
+<p>{name}</p>
+<div class="ui">
+<label>
+<input	type="range" bind:value={size} min={400} max={3000} />
+	{size}&times;{size}px
+</label>
+<label>
+<input	type="range" bind:value={steps} min={3} max={24} />
+		{steps} Columns
+</label>
+<label>
+<input	type="range" bind:value={spread} min={0.1} max={1} step="0.01" />
+		{Math.round(spread*100)}%
+</label>
+<label>
+<input	type="range" bind:value={angle} min={0} max={2*Math.PI} step={math.degToRad(1)} />
+		{Math.round(math.radToDeg(angle))}°
+</label>
+<button on:click={save}>Download Image</button>
+</div>
+
---
+++ src/Marius.svelte
@@ -0,0 +1,83 @@
+<script>
+	import { onMount, afterUpdate } from 'svelte';
+	import {Line, Pt, Create, Group, Bound} from 'pts';
+	import math from 'canvas-sketch-util/math';
+	export let canvas
+	export let size=1024;
+	let context
+	let hue
+	export	let steps = 4;
+	export let name;
+	export let angle = 0.5 * 2 * Math.PI;
+	export let spread = 0.25;
+
+	$: squareSize = size/steps;
+	$: randomDisplacement = squareSize/6;
+	$: rotateMultiplier = 30;
+	
+
+	$: offset = size/12;
+
+	
+	$: name = `marius-${steps}_columns-angle_${Math.round(math.radToDeg(angle))}-spread_${Math.round(spread*100)}-${size}x${size}`
+	
+	let square = (cx=100 , cy=10, size=100, rot=0) => {
+		let sides = 4;
+		let radius = size*Math.sqrt(2) /2;
+		let step =  (2*Math.PI)/sides;
+		let startRadians = (0.25*Math.PI)+ rot;
+		let path = `M ${cx + radius * Math.cos(startRadians)},${cy + radius * Math.sin(startRadians)} `;
+		
+		for (let i = 0; i<= sides; i++ ){
+        const angle = step * i + startRadians;
+				path += `L ${cx + radius * Math.cos(angle)},${cy + radius * Math.sin(angle)} `;		
+    }
+		path +="Z"
+		return new Path2D(path)
+	}
+
+	let render = () =>{
+		context = canvas.getContext('2d');
+		context.fillStyle="#330011";
+		context.lineWidth = size/200;
+
+		context.fillRect(0,0,size,size);
+		
+		let bounds = Bound.fromGroup(Group.fromArray([[offset,offset],[size-offset,size-offset]]));
+
+		let line = Line.fromAngle(bounds.center, angle, size);
+		let linePath = new Path2D(`M${line[0].x} ${line[0].y} L ${line[1].x} ${line[1].y}`)
+		//context.stroke(linePath)
+		
+		let cellSize = bounds.width/steps*0.65;
+		let gridPoints = Create.gridPts (bounds, steps, steps, [0.5,0.5]);
+		
+		gridPoints.map(point =>{
+			context.strokeStyle="#fff";
+			let hue = parseInt(Math.random()*40);
+
+			hue = math.mapRange(point.magnitude(), 0, bounds.bottomRight.magnitude(), 0, 40, true);
+			let lightness = math.mapRange(point.magnitude(), 0, bounds.bottomRight.magnitude(), 30, 60, true);
+			context.fillStyle= `hsl(${hue}, 100%, ${lightness}%)`;
+			let distance = Line.distanceFromPt ( line, point );
+			let variability = math.mapRange(distance, 0, size*spread, 1, 0, true);			
+			let rotation = math.mapRange(Math.random(), 0, 1, variability*0.25*-Math.PI, variability* 0.25*Math.PI, true);
+			let sizeOffset=  math.mapRange(Math.random()*variability, 0, 1, 0, 0 , true);
+
+		  context.fill(square(point.x, point.y,cellSize+sizeOffset,rotation))
+ 			rotation = math.mapRange(Math.random(), 0, 1, variability*0.25*-Math.PI, variability* 0.25*Math.PI, true);			
+			sizeOffset=  math.mapRange(Math.random()*variability, 0, 1, cellSize/2, cellSize/2 , true);
+			
+			context.strokeStyle= `hsla(${hue}, 100%, ${lightness+30}%,${variability})`;
+			
+			
+			context.stroke(square(point.x, point.y,cellSize-sizeOffset,rotation))
+		})
+
+		
+	}
+	
+	onMount(render)
+	afterUpdate(render)
+</script>
+<canvas bind:this={canvas} width={size} height={size}  />
